a160d677310c74beb7c2c816ab2765be4df51332
john gates <john@ubuntu.lan>
2013-10-15 14:54:15 -0700

    Add test atoum and travis

--- src/FTC56/EditorBundle/Parser/Parser.php
+++ src/FTC56/EditorBundle/Parser/Parser.php
@@ -20,44 +20,45 @@ class Parser
 
     private function bbcode($text)
     {
-        $pattern = array('#\[b\](.+)\[\/b\]#isU', '#\[i\](.+)\[\/i\]#isU', '#\[u\](.+)\[\/u\]#isU',
-                                '#\[strike\](.+)\[\/strike\]#isU', '#\[left\](.+)\[\/left\]#isU',
-                                '#\[center\](.+)\[\/center\]#isU', '#\[right\](.+)\[\/right\]#isU',
-                                '#\[justify\](.+)\[\/justify\]#isU', '#\[hr\]#', '#\[quote\](.+)\[\/quote\]#isU',
-                                '#\[quote="(.+)"\](.+)\[\/quote\]#isU', '#\[list(=1)?](.+)[/list]#isU',
-                                '#\[\*\]\s?(.*?)\n#is', '#\[table\](.+)\[/table\]#isU', '#\[tr\](.+)\[\/tr\]#isU',
-                                '#\[td\](.+)\[\/td\]#isU', '#\[code\](.+)\[\/code\]#isU', '#\[hide\](.+)\[\/hide\]#isU',
-                                '#\[img\](.+)\[\/img\]#iU', '#\[img\(([0-9]+)px,([0-9]+)px\)\](.+)\[\/img\]#iU',
-                                '#\[url\](.+)\[\/url\]#iU', '#\[url=(.+)\](.+)\[\/url\]#iU',
-                                '#\[color=(([a-z]+)|(\#[a-f0-9]{3,6}))\](.+)\[\/color\]#isU',
-                                '#\[size=([0-9]+)\](.+)\[\/size\]#isU', '#\[font=(.+)\](.+)\[/font\]#isU',
-                                '#\[sub\](.+)\[\/sub\]#isU', '#\[sup\](.+)\[\/sup\]#isU'
+        $pattern = array(
+            '#\[b\](.+)\[\/b\]#isU' => '<strong>$1</strong>',
+            '#\[i\](.+)\[\/i\]#isU' => '<em>$1</em>',
+            '#\[u\](.+)\[\/u\]#isU' => '<span style="text-decoration:underline;">$1</span>',
+            '#\[strike\](.+)\[\/strike\]#isU' => '<span style="text-decoration:line-through;">$1</span>',
+            '#\[left\](.+)\[\/left\]#isU' => '<div style="text-align:left;">$1</div>',
+            '#\[center\](.+)\[\/center\]#isU' => '<div style="text-align:center;">$1</div>',
+            '#\[right\](.+)\[\/right\]#isU' => '<div style="text-align:right;">$1</div>',
+            '#\[justify\](.+)\[\/justify\]#isU' => '<div style="text-align:justify;">$1</div>',
+            '#\[hr\]#' => '<hr />',
+            '#\[quote\](.+)\[\/quote\]#isU' => '<blockquote><p>$1</p></blockquote>',
+            '#\[quote="(.+)"\](.+)\[\/quote\]#isU' => '<blockquote cite="$1"><p>$2</p></blockquote>',
+            '#\[list(=1)?](.+)[/list]#isU' => '<ul>$2</ul>',
+            '#\[\*\]\s?(.*?)\n#is' => '<li>$1</li>',
+            '#\[table\](.+)\[/table\]#isU' => '<table>$1</table>',
+            '#\[tr\](.+)\[\/tr\]#isU' => '<tr>$1</tr>',
+            '#\[td\](.+)\[\/td\]#isU' => '<td>$1</td>',
+            '#\[code\](.+)\[\/code\]#isU' => '<code>$1</code>',
+            '#\[hide\](.+)\[\/hide\]#isU' => '<span style="display:none;">$1</span>',
+            '#\[img\](.+)\[\/img\]#iU' => '<img src="$1" alt="$1" />',
+            '#\[img\(([0-9]+)px,([0-9]+)px\)\](.+)\[\/img\]#iU' => '<img src="$3" style="width:$1px; height:$2px;" alt="$1" />',
+            '#\[url\](.+)\[\/url\]#iU' => '<a href="$1">$1</a>',
+            '#\[url=(.+)\](.+)\[\/url\]#iU' => '<a href="$1">$2</a>',
+            '#\[color=(([a-z]+)|(\#[a-f0-9]{3,6}))\](.+)\[\/color\]#isU' => '<span style="color:$1;">$4</span>',
+            '#\[size=([0-9]+)\](.+)\[\/size\]#isU' => '<span style="font-size:$1px;">$2</span>',
+            '#\[font=(.+)\](.+)\[/font\]#isU' => '<span style="font-family:$1;">$2</span>',
+            '#\[sub\](.+)\[\/sub\]#isU' => '<sub>$1</sub>',
         );
-        $replace = array('<strong>$1</strong>', '<em>$1</em>',
-                                '<span style="text-decoration:underline;">$1</span>',
-                                '<span style="text-decoration:line-through;">$1</span>',
-                                '<div style="text-align:left;">$1</div>', '<div style="text-align:center;">$1</div>',
-                                '<div style="text-align:right;">$1</div>', '<div style="text-align:justify;">$1</div>',
-                                '<hr />', '<blockquote><p>$1</p></blockquote>',
-                                '<blockquote cite="$1"><p>$2</p></blockquote>', '<ul>$2</ul>', '<li>$1</li>',
-                                '<table>$1</table>', '<tr>$1</tr>', '<td>$1</td>', '<code>$1</code>',
-                                '<span style="display:none;">$1</span>', '<img src="$1" alt="$1" />',
-                                '<img src="$3" style="width:$1px; height:$2px;" alt="$1" />', '<a href="$1">$1</a>',
-                                '<a href="$1">$2</a>', '<span style="color:$1;">$4</span>',
-                                '<span style="font-size:$1px;">$2</span>', '<span style="font-family:$1;">$2</span>',
-                                '<sub>$1</sub>', '<sup>$1</sup>'
-        );
+
         // '#\[spoiler(="(.+)")?\](.+)\[\/spoiler\]#isU',
         // '#\[scroll\](.+)\[\/scroll\]#isU',
         // '#\[updown\](.+)\[\/updown\]#isU',
         // '#\[wow\](.+)\[\/wow\]#isU',
         // '#\[rand\](.+)\[\/rand\]#isU',
 
-        $text = preg_replace($pattern, $replace, $text);
-
         // Paragraphes
-        $text = str_replace('\r', '', $text);
-        $text = "<p>" . preg_replace("/(\n){2,}/", "</p><p>", $text) . "</p>";
+        $text = "<p>" . preg_replace('#\R+#', "</p><p>", $text) . "</p>";
+
+        $text = preg_replace(array_keys($pattern), array_values($pattern), $text);
 
         return $text;
     }
